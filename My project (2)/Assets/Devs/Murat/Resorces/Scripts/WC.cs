4c3a5bd1c9074329c86da88e191812b12c0a3b4e
MuratAr448 <144346044+MuratAr448@users.noreply.github.com>
2025-12-03 12:37:59 +0100

    Update WC.cs

--- My project (2)/Assets/Devs/Murat/Resorces/Scripts/WC.cs
+++ My project (2)/Assets/Devs/Murat/Resorces/Scripts/WC.cs
@@ -1,11 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WC : MonoBehaviour
 {
+    enum flushgame
+    {
+        dirty,
+        doused,
+        clean
+    }
     //spray game
     //flush game
+    flushgame fleshgame;
     bool sprayG = false;
     bool flushG = false;
+    public GameObject flushbutton;
+    public GameObject Twater;
+    public List<Material> durtyness;
+
     void Start()
     {
         
@@ -15,6 +29,36 @@ public class WC : MonoBehaviour
     void Update()
     {
         
+    }
+    void FlushGame()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.gameObject == flushbutton)
+                {
+
+                }
+            }
+        }
+        switch (fleshgame)
+        {
+            case flushgame.dirty:
+                Twater.GetComponent<MeshRenderer>().material = durtyness[0];
+                break;
+            case flushgame.doused:
+                Twater.GetComponent<MeshRenderer>().material = durtyness[1];
+                break;
+            case flushgame.clean: flushG = true;
+                Twater.GetComponent<MeshRenderer>().material = durtyness[2];
+                break;
+            default: break;
+        }
+
+
     }
 
 }

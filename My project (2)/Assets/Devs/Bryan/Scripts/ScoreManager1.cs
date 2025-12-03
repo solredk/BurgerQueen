fc6b0c73f835c03f405760dba26e6727377d1857
Bryansvision <144342080+Bryansvision@users.noreply.github.com>
2025-12-03 12:45:07 +0100

    pushing for score

--- My project (2)/Assets/Devs/Bryan/Scripts/ScoreManager1.cs
+++ My project (2)/Assets/Devs/Bryan/Scripts/ScoreManager1.cs
@@ -7,10 +7,13 @@ public class ScoreManager1 : MonoBehaviour
 {
     public bool takingToLong = false;
     private bool ignoreCustomer = false;
+    private bool nextCustomer;
     private int customerSatisfaction = 10;
     public Slider satisfactionSlider;
     public int score;
     public Customer currentCustomerScript;
+    [SerializeField]
+    private float scoreTimer;
 
     private void Start()
     {
@@ -18,9 +21,12 @@ public class ScoreManager1 : MonoBehaviour
 
     }
 
+   
+
 
     private void Update()
     {
+        scoreTimer += Time.deltaTime;
         currentCustomerScript = FindFirstObjectByType<Customer>();
 
         satisfactionSlider.value = customerSatisfaction;
@@ -30,26 +36,28 @@ public class ScoreManager1 : MonoBehaviour
             StartCoroutine(TookToLong());
         }
 
-        if (currentCustomerScript.currentMood == CustomerMood.Angry && !ignoreCustomer)
-        {
-            GoingDownSatisfaction();
-            ignoreCustomer = true;
-        }
+        //if (currentCustomerScript.currentMood == CustomerMood.Angry && !ignoreCustomer)
+        //{
+        //    GoingDownSatisfaction();
+        //    ignoreCustomer = true;
+        //}
        
 
-        if (currentCustomerScript.hasBeenServed && !ignoreCustomer) {
-            ignoreCustomer = true;
-            switch (currentCustomerScript.currentMood) { 
-                case CustomerMood.Happy:
-                    customerSatisfaction += 2;
-                    break;
-                    case CustomerMood.Neutral:
-                    customerSatisfaction += 1;
-                    break;
+        //if (currentCustomerScript.hasBeenServed && !ignoreCustomer) {
+        //    ignoreCustomer = true;
+        //    switch (currentCustomerScript.currentMood) { 
+        //        case CustomerMood.Happy:
+        //            customerSatisfaction += 2;
+        //            break;
+        //            case CustomerMood.Neutral:
+        //            customerSatisfaction += 1;
+        //            break;
             
-            }
+        //    }
         
-        }
+        //}
+
+
     }
 
     public void GoingDownSatisfaction()
@@ -57,9 +65,22 @@ public class ScoreManager1 : MonoBehaviour
         takingToLong = true;
     }
 
+    public void timeBonus()
+    {
+        if(scoreTimer < 60)
+        {
+            score += 100;
+        }else if (scoreTimer < 300)
+        {
+            score += 50;
+        }
+        else
+        {
+            score += 0;
+        }
+    }
+
    
-
-
     IEnumerator TookToLong()
     {
         customerSatisfaction -= 2;

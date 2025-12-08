92e115d39d0b0b28a979f3af696569ed20a2aab3
Loïc <loic.rutte@gmail.com>
2025-12-08 11:37:24 +0100

    burger order fix

--- My project (2)/Assets/Devs/Loïc/Scripts/PlaatTrigger.cs
+++ My project (2)/Assets/Devs/Loïc/Scripts/PlaatTrigger.cs
@@ -5,6 +5,7 @@ public class PlaatTrigger : MonoBehaviour
 {
     private BurgerAssambleManager m_AssambleManager;
     public GameObject addedIngredient;
+    [SerializeField] float ingredientDistance;
 
     private void Start()
     {
@@ -14,7 +15,7 @@ public class PlaatTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         addedIngredient = other.gameObject;
-        GameObject burgerIng = Instantiate(addedIngredient, new Vector3(-3, 1.55f + m_AssambleManager.burger.Count, -10), Quaternion.identity);
+        GameObject burgerIng = Instantiate(addedIngredient, new Vector3(-3, m_AssambleManager.burger.Count / ingredientDistance + 1.55f, -10), Quaternion.identity);
         burgerIng.transform.localScale = new Vector3(0.3f, 0.06f, 0.3f);
         burgerIng.layer = default;
         burgerIng.GetComponent<Collider>().enabled = false;

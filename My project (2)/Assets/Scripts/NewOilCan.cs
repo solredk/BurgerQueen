3ce41c3fb756b126deb845c9ca838091998bb63a
manaloq <144346195+manaloq@users.noreply.github.com>
2025-12-16 13:27:09 +0100

    Push Update text update
    Push

--- My project (2)/Assets/Scripts/NewOilCan.cs
+++ My project (2)/Assets/Scripts/NewOilCan.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Diagnostics;
 
 public class NewOilCan : MonoBehaviour
 {
     [SerializeField] private GameObject newOilPrefab;
+    [SerializeField] private Material darkBrownMaterial;
     [SerializeField] private float requiredTriggerTime = 3f;
-    [SerializeField] private GameObject newOilCanSpawnPoint;
     [SerializeField] private GameObject frituurObject;
     [SerializeField] private float sceneChangeDelay = 3f;
+    [SerializeField] private SendMessage messageSystem;
 
-    private GameObject currentFrituurObject;
+    private Renderer canRenderer;
     private float triggerTimer = 0f;
     private bool isInFrituur = false;
     private bool hasBeenUsed = false;
-    private bool hasReturnedToSpawn = false;
+    
 
+
+    private void Start()
+    {
+         canRenderer = GetComponent<Renderer>();
+    }
     private void Update()
     {
         if (isInFrituur && !hasBeenUsed)
@@ -32,88 +39,28 @@ public class NewOilCan : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!hasBeenUsed && (other.gameObject.name == "Frituur" || (frituurObject != null && other.gameObject == frituurObject)))
+        if (!hasBeenUsed && other.gameObject == frituurObject)
         {
-            currentFrituurObject = other.gameObject;
             isInFrituur = true;
             triggerTimer = 0f;
-        }
-
-        if (hasBeenUsed && !hasReturnedToSpawn && other.gameObject.name == "NewOilCanSpawnPoint")
-        {
-            hasReturnedToSpawn = true;
-            OnSequenceComplete();
-        }
-
-        if (hasBeenUsed && !hasReturnedToSpawn && newOilCanSpawnPoint != null && other.gameObject == newOilCanSpawnPoint)
-        {
-            hasReturnedToSpawn = true;
-            OnSequenceComplete();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if ((other.gameObject.name == "Frituur" || (frituurObject != null && other.gameObject == frituurObject)) && isInFrituur)
+        if (other.gameObject == frituurObject && isInFrituur)
         {
             isInFrituur = false;
             triggerTimer = 0f;
-            currentFrituurObject = null;
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!hasBeenUsed && (collision.gameObject.name == "Frituur" || (frituurObject != null && collision.gameObject == frituurObject)))
-        {
-            currentFrituurObject = collision.gameObject;
-            isInFrituur = true;
-            triggerTimer = 0f;
-        }
-
-        if (hasBeenUsed && !hasReturnedToSpawn && collision.gameObject.name == "NewOilCanSpawnPoint")
-        {
-            hasReturnedToSpawn = true;
-            OnSequenceComplete();
-        }
-
-        if (hasBeenUsed && !hasReturnedToSpawn && newOilCanSpawnPoint != null && collision.gameObject == newOilCanSpawnPoint)
-        {
-            hasReturnedToSpawn = true;
-            OnSequenceComplete();
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if ((collision.gameObject.name == "Frituur" || (frituurObject != null && collision.gameObject == frituurObject)) && isInFrituur)
-        {
-            isInFrituur = false;
-            triggerTimer = 0f;
-            currentFrituurObject = null;
         }
     }
 
     private void SpawnNewOil()
     {
-        Vector3 spawnPosition;
+        Instantiate(newOilPrefab, frituurObject.transform.position, Quaternion.identity);
+        canRenderer.material = darkBrownMaterial;
+        messageSystem.UpdateMessage("You have completed the replacing the old oil minigame");
 
-        if (frituurObject != null)
-        {
-            spawnPosition = frituurObject.transform.position;
-        }
-        else if (currentFrituurObject != null)
-        {
-            spawnPosition = currentFrituurObject.transform.position;
-        }
-        else
-        {
-            spawnPosition = transform.position;
-        }
-
-        Instantiate(newOilPrefab, spawnPosition, Quaternion.identity);
-
-        
         StartCoroutine(DelayedSceneChange());
     }
 
@@ -123,18 +70,11 @@ public class NewOilCan : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void OnSequenceComplete()
-    {
-
-    }
-
     public void ResetCan()
     {
         hasBeenUsed = false;
-        hasReturnedToSpawn = false;
         isInFrituur = false;
         triggerTimer = 0f;
-        currentFrituurObject = null;
         gameObject.SetActive(true);
     }
 }
